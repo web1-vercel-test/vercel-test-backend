019de2575bc50a832d5f1dba2795d3fc5a8ba09b
amonhaupt <amon.haupt@aol.com>
2025-12-19 09:23:25 +0100

    switch to cors package

--- api/index.js
+++ api/index.js
@@ -1,18 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-app.use(express.json());
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://vercel-test-frontend-ten.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-  next();
-});
+app.use(cors()); 
 
 app.get('/api/ping', (req, res) => res.json({ message: 'Backend connected!' }));
 
